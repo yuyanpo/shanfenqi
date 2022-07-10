ed5490f637913eb116d923821d58bd29d7fb94af
yuyanpo <yuyanpo1021@163.com>
2022-07-10 14:44:33 +0800

    feat: configure theme

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import TDesign from 'tdesign-vue-next';
 // 引入组件库全局样式资源
 import 'tdesign-vue-next/es/style/index.css';
+import './assets/theme/theme.css';
 
 import router from './routers';
 import App from './App.vue';
